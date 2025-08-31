3442cb878c8bd2e21f99d226e552ce57f4b1de4b
matyshh <sharpbarth@gmail.com>
2025-08-31 02:53:27 +0100

    update

--- src/commands/setup.rs
+++ src/commands/setup.rs
@@ -49,7 +49,7 @@ impl SetupCommand {
     }
     
     async fn download_config(&self) -> Result<String> {
-        const CONFIG_URL: &str = "https://gist.githubusercontent.com/mateuphinxx/b34896b41090c7a8c93bdbe2df811be1/raw/opencli.toml";
+        const CONFIG_URL: &str = "https://gist.githubusercontent.com/mateuphinxx/82fd1b1b17ccb23a11fcbe40b83ceaa5/raw/opencli.toml";
         
         let client = reqwest::Client::new();
         let response = client
